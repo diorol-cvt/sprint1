926cfda6d5917b9b7b0d008c269d4b6cbbf45bfb
diorol <danielmihajlov119@gmail.com>
2026-02-12 16:52:48 +0300

    add a

---
+++ src/diorol.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class diorol {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world");
+        double R = 5.0; // инициализация в коде
+        double V = (4.0 / 3.0) * Math.PI * Math.pow(R, 3);
+        System.out.println(V);
+        double x = 2.0;
+        double y = 3.0;
+        double denominator = y + 1.0 / (x * x - 4.0);
+        double fraction = (x + y) / denominator;
+        double result = (1.0 + y) * (2.0 * x + y * y - fraction);
+        System.out.println(result);
+        double q = 1.0;
+        double w = 2.0;
+        double e = 3.0;
+        double res = ((q + 2.0) * w - e) / w + w * e;
+        System.out.println(res);
+        Scanner in = new Scanner(System.in); //github
+//        System.out.println(d);
+    }
+}
