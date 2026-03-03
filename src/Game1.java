b34bd02f0cee662a3695e9228a336e3e95feb902
Diorol_CVT <danielmihajlov119@gmail.com>
2026-03-03 15:22:54 +0300

    add a

--- src/Game1.java
+++ src/Game1.java
@@ -5,8 +5,8 @@ public class Game1 {
         int step = 0;
         int personLive = 3;
         int sizeboard = 5;
-        int personX = 1;
-        int personY = 1;
+        int personX = 0;
+        int personY = 0;
         int castleY = 4;
         Random random = new Random();
         int castleX = random.nextInt(sizeboard);
@@ -17,12 +17,12 @@ public class Game1 {
         String rightBlock = " |";
         String wall = " + —— + —— + —— + —— + —— + ";
         String[][] board = new String[sizeboard][sizeboard];
-        for (int y = 0; y < sizeboard; y++) {
-            for (int x = 0; x < sizeboard; x++) {
-                board[y][x] = "  ";
+        for (int x = 0; x < sizeboard; x++) {
+            for (int y = 0; y < sizeboard; y++) {
+                board[x][y] = "  ";
             }
         }
-        int countMonster = sizeboard * (sizeboard - 1) - 1;
+        int countMonster = sizeboard * (sizeboard - 3) - 1;
         board[castleY][castleX] = castle;
         for (int i = 0; i <= countMonster; i++) {
             board[random.nextInt(sizeboard - 1)][random.nextInt(sizeboard)] = monster;
@@ -38,7 +38,7 @@ public class Game1 {
                 int difficultGame = scanner.nextInt();
                 while (true) {
                     if (difficultGame <= 4 && difficultGame >= 1) {
-                        board[personY - 1][personX - 1] = person;
+                        board[personX][personY] = person;
                         System.out.println("Уровень игры: " + difficultGame);
                         //                                System.out.println(gamingField);
                         for (String[] raw : board) {
@@ -51,42 +51,41 @@ public class Game1 {
                         System.out.println(wall);
 
                         System.out.println("Количество жизней: " + personLive);
-                        System.out.println("Начальные координаты персонажа: X = " + personX + ", Y = " + personY);
+                        System.out.println("Координаты персонажа: X = " + personX + ", Y = " + personY);
                         System.out.println("Введи новые координаты через пробел(Например: 0 1).");
                         int x = scanner.nextInt();
                         int y = scanner.nextInt();
-                        if (x != personX && y != personY || (x <= 0 || y <= 0)) {
+                        if (x != personX && y != personY || (x < 0 || y < 0)) {
                             System.out.println("Такой ход невозможен!");
                         }
                         else {
                             if (Math.abs(personX - x) == 1 || Math.abs(personY - y) == 1) {
-                                if (board[y - 1][x - 1].equals("  ")) {
-                                    board[personY - 1][personX - 1] = "  ";
+                                if (board[x][y].equals("  ")) {
+                                    board[personX][personY] = "  ";
                                     step += 1;
                                     personX = x;
                                     personY = y;
-                                    System.out.println("Номер хода: " + step + "\nНовые координаты: " + personX + ", " + personY);
+                                    System.out.println("Номер хода: " + step + "\nКоординаты персонажа: " + personX + ", " + personY);
                                 } else {
-                                    if (board[y - 1][x - 1].equals(castle)) {
+                                    if (board[x][y].equals(castle)) {
                                         System.out.println("Вы прошли игру!");
                                         break;
                                     } else {
                                         System.out.println("Решите задачу.");
                                     }
                                 }
-                            } else {
+                            }
+                            else {
                                 System.out.println("Координаты не изменились.");
                             }
                             if (personLive <= 0) {
+                                System.out.println("Жизни закончились. Ваш результат: ");
                                 break;
                             }
                         }
-                    } else {
-                        System.out.println("Такого уровня нет!");
-                        break;
                     }
-                    if (personLive <= 0) {
-                        System.out.println("Жизни закончились. Ваш результат: ");
+                    else {
+                        System.out.println("Такого уровня нет!");
                         break;
                     }
                 }
